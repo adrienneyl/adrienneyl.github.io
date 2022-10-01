f0e8bf4bdc462347f685f5990597a8522776f724
Adrienne Leung <adrienne.yl07@gmail.com>
2022-09-30 18:43:00 -0700

    Fixing styling

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -3,14 +3,9 @@ import React from 'react'
 const HeaderSocials = () => {
     return (
         <div className="home__socials">
-            <a href="" className="home__social-link" target="_blank"></a>
-            <i class="fa-brands fa-instagram"></i>
-
-            <a href="" className="home__social-link" target="_blank"></a>
+            <a href="https://github.com/adrienneyl" className="home__social-link" target="_blank"></a>
             <i class="fa-brands fa-github"></i>
-
         </div>
-        
     )
 }
 
