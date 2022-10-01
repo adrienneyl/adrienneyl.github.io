0b071df610ca8de31970c56411d4b514372e40a0
Adrienne Leung <adrienne.yl07@gmail.com>
2022-09-30 18:09:06 -0700

    Home update

---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+    return (
+        <div className="home__socials">
+            <a href="" className="home__social-link" target="_blank"></a>
+            <i class="fa-brands fa-instagram"></i>
+
+            <a href="" className="home__social-link" target="_blank"></a>
+            <i class="fa-brands fa-github"></i>
+
+            <a href="" className="home__social-link" target="_blank"></a>
+            <i class="fa-brands fa-instagram"></i>
+        </div>
+
+
+        
+    )
+}
+
+export default HeaderSocials
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import "./home.css";
 import Me from "../../assets/avatar-1.svg";
+import HeaderSocials from './HeaderSocials';
+import ScrollDown from './ScrollDown';
 
 const Home = () => {
     return (
@@ -9,6 +11,12 @@ const Home = () => {
                 <img src={Me} alt="" className="home__img" />
                 <h1 className="home__name">Bolby Doe</h1>
                 <span className="home__education">I'm a Front-End developer</span>
+            
+                <HeaderSocials />
+
+                <a href="#contact" className="btn">Hire Me</a>
+
+                <ScrollDown />
             </div>
         </section>
     )
