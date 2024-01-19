7800549b7bdb9af130f7d6dfced4ab4a0eb355dc
adrienneyl <adrienne.yl07@gmail.com>
2024-01-18 22:11:08 -0800

    Mobile styling changes for front-page and footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,8 +5,8 @@ function Footer() {
   return (
     <div className="footer">
       <div className="footer-content">
-        <p>© 2024 Adrienne Leung</p>
-        <p>Designed and developed from scratch with endless music and matcha.</p>
+        <p>© 2024 Adrienne Leung <br /> 
+        Designed and developed from scratch with endless music and matcha.</p>
       </div>
     </div>
 
