1e358815c6592c233c7795ef215bcbd4e7039343
adrienneyl <adrienne.yl07@gmail.com>
2026-09-08 21:32:33 -0700

    basic navbar works

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// COMPONENT IMPORTS
+import { Home } from './pages/home/home';
+import { Archive } from './pages/archive/archive';
+import { About } from './pages/about/about';
+import { Set15 } from './pages/case-studies/set15/set15';
+import { Set17 } from './pages/case-studies/set17/set17';
+import { Set18 } from './pages/case-studies/set18/set18';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'archive',
+    component: Archive,
+  }
+  ,
+  {
+    path: 'about',
+    component: About,
+  },
+  {
+    path: 'case-study/set15',
+    component: Set15,
+  },
+    {
+    path: 'case-study/set17',
+    component: Set17,
+  },
+    {
+    path: 'case-study/set18',
+    component: Set18,
+  }
+];
--- portfolio/src/app/app.ts
+++ portfolio/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// COMPONENT IMPORTS
+import { Navbar } from './components/navbar/navbar';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
